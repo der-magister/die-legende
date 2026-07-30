fde4fc98c6d9c443c3e9b94d28d7bdb0b57d79d4
Magister <heiko.wolf.mail@e.email>
2026-07-30 11:19:04 +0200

    30.07.2026 11:19 Uhr Update

---
+++ globals.c
@@ -0,0 +1,42 @@
+//   Die Legende
+//
+//   Copyright (C) 2026 Heiko Wolf
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License As published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY Or FITNESS For A PARTICULAR PURPOSE.  See the
+//   GNU General Public License For more details.
+//
+//   You should have received a copy of the GNU General Public License along
+//   With this program; if not, write to the Free Software Foundation, Inc.,
+//   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+//   Kontakt: heiko.wolf.mail@gmail.com
+
+//   File: globals.c
+
+uint8_t v_hp;					//HP (Lebenspunkte)
+uint8_t v_mhp;					//maximale HP der aktuellen Spielerstufe
+uint8_t v_mp;					//MP (Manapunkte)
+uint8_t v_mmp;					//maximale MP der aktuellen Spielerstufe
+uint8_t v_st;					//Erfahrungsstufe des Spielers
+uint8_t v_erf;					//Erfahrungspunkte des Spielers
+
+uint8_t v_atw;					//Attackewert des Spielers
+uint8_t v_abw;					//Abwehrwert des Spielers
+
+uint8_t v_go;					//Anzahl Goldmünzen Spieler
+uint8_t v_mgo;					//maximale Anzahl Goldmünzen
+uint8_t v_mht;					//Anzahl Heiltränke Spieler
+uint8_t v_mht;					//maximale Heiltränke Spieler
+uint8_t v_mt;					//Anzahl Manatränke Spieler
+uint8_t v_mmt;					//maximale Manatränke Spieler
+
+
+
+
